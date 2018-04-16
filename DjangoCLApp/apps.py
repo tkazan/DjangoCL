a3b43f1893ea8ec5aed6dda5ca4149168e9e6b48
Tobiasz Kazanowski <tobiaszkazanowski@gmail.com>
2018-04-16 22:12:30 +0200

    initial commit DjangoCL

---
+++ DjangoCLApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoclappConfig(AppConfig):
+    name = 'DjangoCLApp'
---
+++ DjangoVirtEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tobiasz/Pulpit/Kurs_Python/DjangoCL/DjangoVirtEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
